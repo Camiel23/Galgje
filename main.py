c6db888dbc404d5ac341bacabb103c8b7d52687a
Camiel van der Marel <110407@st-maartenscollege.nl>
2020-06-12 11:24:06 +0000

    woordenlijst en kiezen

---
+++ main.py
@@ -0,0 +1,6 @@
+woordenlijst = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", "heesterperk"]
+woord = random.choice(woordenlijst)
+lengtewoord = len(woord)
+
+print("intro-bericht")
+print("het woord heeft " + str(lengtewoord) + " letters")
