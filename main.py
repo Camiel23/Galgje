dcbb15153c3dc2f936ae632b474d9b635cf5c335
Camiel van der Marel <110407@st-maartenscollege.nl>
2020-06-12 11:44:47 +0000

    woord wordt geraden toegevoegd

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ lengtewoord = len(woord)
 
 print("Hallo, welkom bij galgje! Raad letters en als je het hele woord weet, typ het woord helemaal om te winnen. Succes en veel plezier!")
 print("het woord heeft " + str(lengtewoord) + " letters")
+
+while True: #woord wordt geraden
+  userguess = (input(": "))
+  match = re.search(userguess, woord)
+  if userguess == woord
+    print("je hebt het woord" + "'" + woord = "'" + "geraden")
+    break
+  
+  
+
