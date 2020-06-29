f9648fe61fa46d4c45fb58ded4dc2965fc26a181
Camiel van der Marel <110407@st-maartenscollege.nl>
2020-06-29 14:22:40 +0000

    code voor letters geraden toegevoegd. code voor letters fout geraden toegevoegd. hele woord geraden anders gedaan.

--- main.py
+++ main.py
@@ -8,50 +8,79 @@ lengtewoord = len(woord)
 print("Hallo, welkom bij galgje! Raad letters en als je het hele woord weet, typ het woord helemaal om te winnen. Succes en veel plezier!")
 print("het woord heeft " + str(lengtewoord) + " letters")
 
-        print "_________"
-				print "|	 |"
-				print "|	 O"
-				print "|"
-				print "|"
-				print "|"
-				print "|________"
-		elif (guesses == 1):
-				print "_________"
-				print "|	 |"
-				print "|	 O"
-				print "|	 |"
-				print "|	 |"
-				print "|"
-				print "|________"
-		elif (guesses == 2):
-				print "_________"
-				print "|	 |"
-				print "|	 O"
-				print "|	\|"
-				print "|	 |"
-				print "|"
-				print "|________"
-		elif (guesses ==3):
-				print "_________"
-				print "|	 |"
-				print "|	 O"
-				print "|	\|/"
-				print "|	 |"
-				print "|"
-				print "|________"
-		elif (guesses == 4):
-				print "_________"
-				print "|	 |"
-				print "|	 O"
-				print "|	\|/"
-				print "|	 |"
-				print "|	/ "
-				print "|________"
-		elif (guesses == 5):
-				print "_________"
-				print "|	 |"
-				print "|	 O"
-				print "|	\|/"
-				print "|	 |"
-				print "|	/ \ "
-				print "|________" 
+
+guesses = ""
+
+turns = 5
+
+while turns > 0:
+  verkeerd = 0
+  for letter in woord:
+      if letter in guess:
+        print (letter)
+      else: 
+        print("_")
+        verkeerd = verkeerd + 1
+  if failed == 0:
+    print("Gefeliciteerd, je hebt in" + str(guesses) + "gewonnen!")
+  guess = input("Raad een letter: ")
+  guesses = guesses + guess
+  if guess not in woord:
+    turns = turns - 1
+    print("Jammer, fout...")
+    print("Je hebt nog" + turns + "gokkansen over.")
+    
+    if turns == 5:
+      print ("_________")
+      print ("|	 |")
+      print ("|	 O")
+      print ("|")
+      print ("|")
+      print ("|")
+      print ("|________")
+    if turns == 4:
+      print ("_________")
+	    print ("|  |")
+	    print ("|	 O")
+	    print ("|	 |")
+	    print ("|	 |")
+	    print ("|")
+	    print ("|________")
+
+    if turns == 3:
+    	print ("_________")
+			print ("|	 |")
+			print ("|	 O")
+			print ("|	\|")
+  		print ("|	 |")
+			print ("|")
+			print ("|________")
+
+    if turns == 2:
+    	print ("_________")
+    	print ("|	 |")
+			print ("|	 O")
+			print ("|	\|/")
+			print ("|	 |")
+			print ("|")
+			print ("|________")
+
+    if turns == 1:
+      print ("_________")
+			print ("|	 |")
+			print ("|	 O")
+			print ("|	\|/")
+			print ("|	 |")
+			print ("|	/ ")
+			print ("|________")
+
+    if turns == 0:
+      print ("_________")
+			print ("|	 |")
+			print ("|	 O")
+			print ("|	\|/")
+			print ("|	 |")
+			print ("|	/ \ ")
+			print ("|________") 
+      print("Helaas, je hebt verloren. Het woord was:" + woord )
+
